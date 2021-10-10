1b4f9572b9ee4e189103c678e5b76aa5751959ab
PRITAM RAJ <31183383+rpritam010@users.noreply.github.com>
2021-10-10 21:25:32 +0530

    Update User.java

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/User/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/User/User.java
@@ -1,10 +1,12 @@
 package com.in28minutes.rest.webservices.restfulwebservices.User;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
@@ -26,6 +28,9 @@ public class User {
 	@Past
 	@ApiModelProperty(notes="Name should have atlest 2 characters")
 	private Date birthDate;
+	
+	@OneToMany(mappedBy = "User")
+	private List<Post> posts;
 	
 	protected User() {
 		
@@ -54,6 +59,17 @@ public class User {
 	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
 	}
+	
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
+	
 
 	@Override
 	public String toString() {
