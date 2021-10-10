212d22c558fe7b27f41ae772b4fc73ef8d6368ce
PRITAM RAJ <31183383+rpritam010@users.noreply.github.com>
2021-10-10 21:30:05 +0530

    Update data.sql
    added the data

--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -1,3 +1,4 @@
 insert into User values(1001, SYSDATE(),'AB');
 insert into USER values(1002, SYSDATE(),'ST');
 insert into USER values(1003, SYSDATE(),'VK');
+insert into Post values(11001,'My First Post',1001);
